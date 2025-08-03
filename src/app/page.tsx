0de247aa83c76a54ea001e2c9b80cd0c231ab2b6
Mahedul-Karim <karimrupo21@gmail.com>
2025-08-03 17:26:37 +0600

    gradient utility class added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
 export default function Home() {
   return (
-    <main
-      className="bg-cover bg-no-repeat bg-center"
-      style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/bg.jpg')`,
-      }}
-    >
-      <div
-        className="size-50 border-border border text-white bg-glass"
-       
-      >
-        fvzdgsgd
-      </div>
-    </main>
+    <div className="border-border border text-white bg-glass">
+      <span className="text-7xl font-bold text-aurora">HEllow Workd</span>
+    </div>
   );
 }
