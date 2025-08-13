1664d4766fa7d9fbfa939ea9a4b759fad5767f9d
Mahedul-Karim <karimrupo21@gmail.com>
2025-08-13 17:46:11 +0600

    education section updated

--- src/components/common/Spotlight.tsx
+++ src/components/common/Spotlight.tsx
@@ -2,15 +2,13 @@
 
 import { useRef } from "react";
 
-interface SpotlightCardProps extends React.PropsWithChildren {
+interface SpotlightCardProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
-  spotlightColor?: `rgba(${number}, ${number}, ${number}, ${number})`;
 }
 
 const SpotlightCard: React.FC<SpotlightCardProps> = ({
   children,
   className = "",
-  spotlightColor = "rgba(255, 255, 255, 0.25)",
 }) => {
   const divRef = useRef<HTMLDivElement>(null);
 
@@ -23,7 +21,6 @@ const SpotlightCard: React.FC<SpotlightCardProps> = ({
 
     divRef.current.style.setProperty("--mouse-x", `${x}px`);
     divRef.current.style.setProperty("--mouse-y", `${y}px`);
-    divRef.current.style.setProperty("--spotlight-color", spotlightColor);
   };
 
   return (
--- src/components/resume/Education.tsx
+++ src/components/resume/Education.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import SpotlightCard from "../common/Spotlight";
+import { Badge } from "../ui/badge";
 
 const Education = ({
   educations = [],
@@ -14,20 +16,29 @@ const Education = ({
     <div className="flex flex-col gap-6 mt-8">
       {educations?.length > 0 &&
         educations?.map((edu, i) => (
-          <div key={i} className="grid grid-cols-[16px_1fr] gap-6 text-white">
-            <div className="relative">
-              {i !== educations.length - 1 && <div className="absolute top-4 left-[50%] -translate-x-[50%] w-[1px] bg-white -bottom-[24px]"></div>}
-              <div className="size-4 bg-black border-3 border-solid border-secondary rounded-full"></div>
-            </div>
-            
-            <div className="flex justify-between gap-2 grow -mt-[5px]">
-              <div className="flex flex-col">
-                <h2 className="text-xl text-white font-bold">{edu.degree}</h2>
-                <p className="text-base text-white/60">{edu.university}</p>
+          <SpotlightCard
+            key={i}
+            className="rounded-2xl hover:bg-glass border border-transparent hover:border-border/20 transition-all duration-300 overflow-x-clip"
+          >
+            <div className="grid grid-cols-[16px_1fr] gap-6 text-white px-4 py-6">
+              <div className="relative">
+                {i !== educations.length - 1 && (
+                  <div className="absolute top-4 left-[50%] -translate-x-[50%] w-[1px] bg-white/60 -bottom-[74px]"></div>
+                )}
+                <div className="size-4 bg-black border-3 border-solid border-secondary rounded-full"></div>
               </div>
-              {/* <div>World</div> */}
+
+              <div className="flex flex-col sm:flex-row justify-between gap-2 grow -mt-[5px]">
+                <div className="flex flex-col">
+                  <h2 className="text-lg xs:text-xl text-white font-semibold">{edu.degree}</h2>
+                  <p className="text-sm xs:text-base text-white/60">{edu.university}</p>
+                </div>
+                <div>
+                  <Badge className="rounded-full xs:text-sm text-white bg-glass px-2 xs:px-4 py-1 xs:py-1.5">{edu.period}</Badge>
+                </div>
+              </div>
             </div>
-          </div>
+          </SpotlightCard>
         ))}
     </div>
   );
